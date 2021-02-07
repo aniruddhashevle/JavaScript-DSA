67814e43c95d6e1f072ad407ca2788c90ab14c02
Aniruddha Shevle <aniruddhashevle@gmail.com>
2021-02-07 18:01:17 +0800

    feat: delete nodes

--- Tree/binarySearchTree.js
+++ Tree/binarySearchTree.js
@@ -46,6 +46,15 @@ class BST {
         }
     }
 
+    /**
+     * 
+     * @param {Node} parentRef 
+     * @param {Node|null} ref
+     * @param {number} value
+     * @param {boolean} isLeftNode
+     * 
+     * @returns {{parentRefNode: Node, node: Node, isLeftNode: boolean}}
+     */
     findNode(parentRef, ref, value, isLeftNode = false) {
         var node = parentRefNode = null;
         if (ref) {
@@ -68,21 +77,78 @@ class BST {
         return ref.left === null && ref.right === null;
     }
 
+    makeParentsChildEmpty(parent, isLeftNode) {
+        if (isLeftNode) {
+            parent.left = null;
+        } else {
+            parent.right = null;
+        }
+    }
+
+    findSmallestRightNode(node, parentNode, isLeftNode = false) {
+        if (!parentNode) parentNode = node;
+        if (node.left) {
+            var { parentOfNodeToDelete, nodeToDelete, isLeftNode } = this.findSmallestRightNode(node.left, node, true);
+        } else if (node.right) {
+            var { parentOfNodeToDelete, nodeToDelete, isLeftNode } = this.findLargestLeftNode(node.right, node, false);
+        }
+        return {
+            parentOfNodeToDelete: parentOfNodeToDelete || parentNode,
+            nodeToDelete: nodeToDelete || node,
+            isLeftNode
+        };
+    }
+
+    findLargestLeftNode(node, parentNode, isLeftNode = true) {
+        if (!parentNode) parentNode = node;
+        if (node.right) {
+            var { parentOfNodeToDelete, nodeToDelete, isLeftNode } = this.findLargestLeftNode(node.right, node, false);
+        } else if (node.left) {
+            var { parentOfNodeToDelete, nodeToDelete, isLeftNode } = this.findSmallestRightNode(node.left, node, true);
+        }
+        return {
+            parentOfNodeToDelete: parentOfNodeToDelete || parentNode,
+            nodeToDelete: nodeToDelete || node,
+            isLeftNode
+        };
+    }
+
     delete(value) {
         var { parentRefNode, node, isLeftNode } = this.findNode(this.root, this.root, value);
+        var nodeValToBeDeleted = null;
         if (node) {
             if (this.isLeafNode(node)) {
-                if (isLeftNode) {
-                    parentRefNode.left = null;
-                } else {
-                    parentRefNode.right = null;
-                }
+                nodeValToBeDeleted = node.value;
+                this.makeParentsChildEmpty(parentRefNode, isLeftNode);
             } else {
-                // TODO: delete non-leaf node
+                if (node.right) {
+                    var { parentOfNodeToDelete, nodeToDelete, isLeftNode } = this.findSmallestRightNode(node);
+                } else {
+                    var { parentOfNodeToDelete, nodeToDelete, isLeftNode } = this.findLargestLeftNode(node);
+                }
+                console.log(parentOfNodeToDelete.value, nodeToDelete.value, isLeftNode);
+                // nodeToDelete will always be a leaf nodeF
+                nodeValToBeDeleted = node.value;
+                node.value = nodeToDelete.value;
+                this.makeParentsChildEmpty(parentOfNodeToDelete, isLeftNode);
             }
+            return nodeValToBeDeleted;
         } else {
             console.log('Node not found!');
         }
     }
-
 }
+
+var bst = new BST();
+bst.insert(10);
+bst.insert(15);
+bst.insert(5);
+bst.insert(8);
+bst.insert(20);
+bst.insert(18);
+bst.insert(13);
+bst.insert(7);
+bst.insert(9);
+bst.root;
+bst.delete(20);
+bst.root;
